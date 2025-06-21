9d823461d0e140ba93401be32b265a9e2984d345
David P. Chassin <david.chassin@me.com>
2025-06-21 09:57:22 -0700

    Performance improvement and immigrant fix

--- population.py
+++ population.py
@@ -7,7 +7,7 @@ class Population:
 
     # children are ages 0-20
     adult_age = 25 # adults are ages 25-65
-    immigrant_age = 45 # immigrants age 25-45
+    immigrant_ages = list(range(20,45,5)) # immigrants age 20-45
     senior_age = 70 # seniors are ages 70-105
 
     def __init__(self,country,year,run_to=None,immigration=0.675,fertility_change_rate=-0.001):
@@ -22,7 +22,7 @@ class Population:
         """        
         self.year = year
         self.country = country.upper()
-        self.data = pd.read_csv(f"{country.lower()}{year}.csv",index_col="Age").fillna(0)
+        self.data = pd.read_csv(f"{country.lower()}{year}.csv",index_col="Age",dtype=np.float64).fillna(0)
         self.data["Year"] = year
         self.immigration = immigration*1e6 if immigration else 0
         self.fertility_change_rate = fertility_change_rate
@@ -43,7 +43,7 @@ class Population:
 
     def __getitem__(self,*args):
         
-        return self.data.loc[*args[0]]
+        return self.data.loc[args[0]]
 
     def run_to(self,year):
         result = [self.data]
@@ -56,15 +56,14 @@ class Population:
             adult_total = adult_male + adult_female
             male_fraction = adult_male / adult_total
             female_fraction = adult_female / adult_total
-            age_groups = ( self.immigrant_age - self.adult_age ) / 5
+            # age_groups = ( self.immigrant_ages - self.adult_age ) / 5
 
-            data["Male"] = data["Male+1y"]
-            data["Female"] = data["Female+1y"]
+            data["Immigrants/y"] = 0
+            data.loc[self.immigrant_ages,"Immigrants/y"] = round(self.immigration / len(self.immigrant_ages))
+            data["Male"] = data["Male+1y"] + data["Immigrants/y"] * male_fraction
+            data["Female"] = data["Female+1y"] + data["Immigrants/y"] * female_fraction
 
             data["Fertility Rate"] = data["Fertility Rate"]*(1+self.fertility_change_rate)**5
-
-            data.loc[self.adult_age:self.immigrant_age,"Male"] += round(male_fraction * self.immigration * 5 / age_groups)
-            data.loc[self.adult_age:self.immigrant_age,"Female"] += round(female_fraction * self.immigration * 5 / age_groups)
 
             data["Male births/y"] = round(data["Female"]*data["Fertility Rate"]*data["MF Birth Ratio"]/2)
             data["Female births/y"] = round(data["Female"]*data["Fertility Rate"]/data["MF Birth Ratio"]/2)
@@ -74,11 +73,10 @@ class Population:
             data.loc[105,"Male deaths/y"] = data.loc[105,"Male"]
             data.loc[105,"Female deaths/y"] = data.loc[105,"Female"]
             
-            data.loc[5,"Male+1y"] = round(data.loc[5,"Male"]*0.8 + data["Male births/y"].sum() - data.loc[5,"Male deaths/y"])
-            data.loc[5,"Female+1y"] = round(data.loc[5,"Female"]*0.8 + data["Female births/y"].sum() - data.loc[5,"Female deaths/y"])
-            for age in data.index[1:-1]:
-                data.loc[age,"Male+1y"] = round(data.loc[age,"Male"]*0.8 + data.loc[age-5,"Male"]*0.2 - data.loc[age,"Male deaths/y"])
-                data.loc[age,"Female+1y"] = round(data.loc[age,"Female"]*0.8 + data.loc[age-5,"Female"]*0.2 - data.loc[age,"Female deaths/y"])
+            data.loc[5,"Male+1y"] = max(0,round(data.loc[5,"Male"]*0.8 + (data["Male births/y"].sum() - data.loc[5,"Male deaths/y"])*5))
+            data.loc[5,"Female+1y"] = max(0,round(data.loc[5,"Female"]*0.8 + (data["Female births/y"].sum() - data.loc[5,"Female deaths/y"])*5))
+            data.loc[10:,"Male+1y"] =   round(data.loc[10:,"Male"]*0.8 + data.loc[:105,"Male"]*0.2 - data.loc[10:,"Male deaths/y"]*5).clip(lower=0)
+            data.loc[10:,"Female+1y"] = round(data.loc[10:,"Female"]*0.8 + data.loc[:105,"Female"]*0.2 - data.loc[10:,"Female deaths/y"]*5).clip(lower=0)
 
             result.append(data)
 
@@ -112,7 +110,11 @@ class Population:
 
 if __name__ == "__main__":
 
-    test = Population("usa",2020,2100)
+    pd.options.display.max_columns = None
+    pd.options.display.width = None
+    pd.options.display.max_rows = None
+
+    test = Population("usa",2020,2100,immigration=1.0)
     test.to_csv("population.csv")
 
     plt.clf()
@@ -124,6 +126,7 @@ if __name__ == "__main__":
         legend=True,
         )
     plt.savefig("population.png")
+    print(pop)
     
     plt.clf()
     pop,fig2 = test.plot_dependents(
